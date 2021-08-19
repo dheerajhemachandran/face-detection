a5f99cde2e98a5098ca9c3efd782e5a23923636d
dheeraj <80874729+dheerajhemachandran@users.noreply.github.com>
2021-08-19 09:48:31 +0530

    Update face.py

--- face.py
+++ face.py
@@ -1,7 +1,6 @@
-
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
 cap = cv2.VideoCapture(0)
 
