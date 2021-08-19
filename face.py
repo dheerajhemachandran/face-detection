48894849d8fa2a5ef4b36d33bb67147dd6b2bcbc
dheeraj <80874729+dheerajhemachandran@users.noreply.github.com>
2021-08-19 09:44:20 +0530

    Update face.py

--- face.py
+++ face.py
@@ -2,11 +2,8 @@
 import cv2
 
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
-
 
 cap = cv2.VideoCapture(0)
-
 
 while 1:
 
@@ -22,17 +19,7 @@ while 1:
 	for (x,y,w,h) in faces:
 		
 		cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
-		roi_gray = gray[y:y+h, x:x+w]
-		roi_color = img[y:y+h, x:x+w]
-
 		
-		eyes = eye_cascade.detectMultiScale(roi_gray)
-
-		
-		for (ex,ey,ew,eh) in eyes:
-			cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
-
-	
 	cv2.imshow('img',img)
 
 	
